45140163e5fb0e38e91d08981ea3d326d0c268b3
Tomasz Gadzinski <tomasz.gadzinski04@gmail.com>
2026-05-06 15:40:18 +0200

    feat: add album and asset abstract repositories

---
+++ lib/data/repositories/album/album_repository.dart
@@ -0,0 +1,10 @@
+import "../../../domain/album/album.dart";
+import "../../../domain/album/album_preview.dart";
+import "../../../utils/result.dart";
+
+abstract class AlbumRepository {
+  Future<Result<List<AlbumPreview>>> getAlbumPreviews();
+  Future<Result<Album>> getAlbum(String id);
+  Future<Result<void>> create(Album album);
+  Future<Result<void>> delete(String id);
+}
---
+++ lib/data/repositories/asset/asset_repository.dart
@@ -0,0 +1,11 @@
+import "../../../utils/result.dart";
+import "../../../domain/asset/asset.dart";
+
+abstract class AssetRepository {
+  Future<Result<List<Asset>>> getAssets();
+  Future<Result<Asset>> getAsset(String id);
+  Future<Result<void>> create(Asset asset);
+  Future<Result<void>> delete(Asset asset);
+  Uri getThumbnailUri(Asset asset);
+  Uri getOriginalUri(Asset asset);
+}
