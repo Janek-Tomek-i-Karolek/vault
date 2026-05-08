e2db08fa9b9edcde6aeba806530521fe14bfa3b4
Kyloof <jankubow.sr@gmail.com>
2026-05-08 18:38:51 +0200

    material theme added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:vault/features/auth/presentation/view/login_screen.dart';
 import 'package:vault/features/auth/presentation/view/register_screen.dart';
 import 'package:vault/features/auth/presentation/viewmodel/auth_view_model.dart';
+import 'package:vault/theme/material_theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,26 +15,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+	final TextTheme textTheme = Theme.of(context).textTheme;
+    final MaterialTheme materialTheme = MaterialTheme(textTheme);
+
     return MaterialApp(
       title: 'vault',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+	  theme: materialTheme.light(),
+	  darkTheme: materialTheme.dark(),
+	  themeMode: ThemeMode.system;
       home: ChangeNotifierProvider(
         create: (context) => AuthViewModel(),
         child: const RegisterScreen(),
