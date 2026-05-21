8114390fc0cc64f085c8a3f63a26337776d1251a
Kyloof <jankubow.sr@gmail.com>
2026-05-21 14:34:37 +0200

    WIP: pre-merge commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vault/data/repositories/album/mock_album_repository.dart';
 import 'package:vault/data/repositories/asset/mock_asset_repository.dart';
-import 'package:vault/data/repositories/vault/vault_repository.dart';
+import 'package:vault/data/repositories/connection/connection_repository.dart';
 import 'package:vault/ui/features/albums/viemodel/albums_viewmodel.dart';
 import 'package:vault/ui/features/albums/view/albums_screen.dart';
 import 'package:vault/ui/features/auth/view/login_screen.dart';
@@ -10,8 +10,8 @@ import 'package:vault/ui/features/auth/view/register_screen.dart';
 import 'package:vault/ui/features/auth/viewmodel/auth_view_model.dart';
 import 'package:vault/ui/core/theme/material_theme.dart';
 import 'package:vault/ui/features/user/view/profile_screen.dart';
-import 'package:vault/ui/features/vault/view/connecton_screen.dart';
-import 'package:vault/ui/features/vault/viewmodel/vault_viewmodel.dart';
+import 'package:vault/ui/features/connection/view/connection_screen.dart';
+import 'package:vault/ui/features/connection/viewmodel/connection_viewmodel.dart';
 import 'package:vault/utils/enums/connection_status.dart';
 import 'package:vault/utils/result.dart';
 import 'package:vault/utils/secure_storage.dart';
@@ -32,8 +32,9 @@ class MyApp extends StatelessWidget {
       providers: [
         //ChangeNotifierProvider(create: (_) => AuthViewModel()),
         ChangeNotifierProvider(
-          create: (_) =>
-              VaultViewModel(vaultRepository: ImmichVaultRepository()),
+          create: (_) => ConnectionViewModel(
+            connectionRepository: ImmichConnectionRepository(),
+          ),
         ),
         ChangeNotifierProvider(
           create: (_) => AlbumsViewModel(
@@ -47,13 +48,13 @@ class MyApp extends StatelessWidget {
         theme: materialTheme.light(),
         darkTheme: materialTheme.dark(),
         themeMode: ThemeMode.system,
-        home: Selector<VaultViewModel, ConnectionStatus>(
+        home: Selector<ConnectionViewModel, ConnectionStatus>(
           selector: (_, viewModel) => viewModel.status,
           builder: (_, status, _) {
             return switch (status) {
               ConnectionStatus.loading => const CircularProgressIndicator(),
               ConnectionStatus.connected => const AlbumsScreen(),
-              ConnectionStatus.disconnected => const ConnectonScreen(),
+              ConnectionStatus.disconnected => const ConnectionScreen(),
             };
           },
         ),
@@ -61,94 +62,8 @@ class MyApp extends StatelessWidget {
           "/login": (context) => LoginScreen(),
           "/register": (context) => RegisterScreen(),
           "/albums": (context) => AlbumsScreen(),
-          "/connect": (context) => ConnectonScreen(),
+          "/connect": (context) => ConnectionScreen(),
         },
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
